4daed9c5729a69fc60290435a75773b4a48591a2
cloudwebrtc <duanweiwei1982@gmail.com>
2021-01-13 14:35:45 +0800

    rename

--- pkg/media/rtp/udp_test.go
+++ pkg/media/rtp/udp_test.go
@@ -5,7 +5,7 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/cloudwebrtc/go-sip-ua/pkg/media/transport/rtp"
+	"github.com/cloudwebrtc/go-sip-ua/pkg/media/rtp"
 
 	"github.com/ghettovoice/gosip/log"
 	"github.com/sirupsen/logrus"
